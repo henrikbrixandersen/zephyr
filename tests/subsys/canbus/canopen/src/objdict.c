8ee03195a9a9441eecfa11e3ad9206b8c15097c6
Henrik Brix Andersen <henrik@brixandersen.dk>
2021-11-15 22:42:01 +0100

    test: canbus: canopen: add test suite for the native CANopen stack
    Add unit tests for the native CANopen communication stack.
    
    Signed-off-by: Henrik Brix Andersen <henrik@brixandersen.dk>

---
+++ tests/subsys/canbus/canopen/src/objdict.c
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2021-2025 Henrik Brix Andersen <henrik@brixandersen.dk>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/canbus/canopen/od.h>
+#include <zephyr/types.h>
+
+#include "objdict.h"
+
+CANOPEN_OD_DEFINE_OBJECTS(objdict,
+	/* 1000h - Device type */
+	CANOPEN_OD_OBJECT_ENTRIES(0x1000U,
+		CANOPEN_OD_ENTRY_UNSIGNED32(0U, &(uint32_t){ 0U }, NULL, NULL,
+					CANOPEN_OD_ATTR_ACCESS_READ_ONLY),
+	),
+	/* 1001h - Error register */
+	CANOPEN_OD_OBJECT_ENTRIES(0x1001U,
+		CANOPEN_OD_ENTRY_UNSIGNED32(0U, &(uint32_t){ 0U }, NULL, NULL,
+					CANOPEN_OD_ATTR_ACCESS_READ_ONLY),
+	),
+	/* 1018h - Identity object */
+	CANOPEN_OD_OBJECT_ENTRIES(0x1018U,
+		CANOPEN_OD_ENTRY_UNSIGNED8(0U, &(uint8_t){ 4U }, NULL, NULL,
+					CANOPEN_OD_ATTR_ACCESS_READ_ONLY),
+		CANOPEN_OD_ENTRY_UNSIGNED32(1U, &(uint32_t){ 0U }, NULL, NULL,
+					CANOPEN_OD_ATTR_ACCESS_READ_ONLY),
+		CANOPEN_OD_ENTRY_UNSIGNED32(2U, &(uint32_t){ 0xdeadbeefU }, NULL, NULL,
+					CANOPEN_OD_ATTR_ACCESS_READ_ONLY),
+		CANOPEN_OD_ENTRY_UNSIGNED32(3U, &(uint32_t){ 42U }, NULL, NULL,
+					CANOPEN_OD_ATTR_ACCESS_READ_ONLY),
+		CANOPEN_OD_ENTRY_UNSIGNED32(4U, &(uint32_t){ 1331U }, NULL, NULL,
+					CANOPEN_OD_ATTR_ACCESS_READ_ONLY),
+	),
+);
---
+++ tests/subsys/canbus/canopen/src/test_canopen_od.c
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2021-2025 Henrik Brix Andersen <henrik@brixandersen.dk>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/canbus/canopen/od.h>
+#include <zephyr/ztest.h>
+
+#include "objdict.h"
+
+ZTEST(canopen_od, test_canopen_od_find)
+{
+	canopen_od_handle_t handle;
+
+	handle = canopen_od_find(&objdict, 0x1010U, 0U);
+	zassert_false(canopen_od_handle_is_valid(handle), "Object 1010h not present");
+
+	handle = canopen_od_find(&objdict, 0x1000U, 0U);
+	zassert_true(canopen_od_handle_is_valid(handle), "Object 1000h is present");
+
+	handle = canopen_od_find(&objdict, 0x1001U, 0U);
+	zassert_true(canopen_od_handle_is_valid(handle), "Object 1001h is present");
+	/* zassert_equal(obj->index, 0x1001, "Object index is 1001h"); */
+
+	handle = canopen_od_find(&objdict, 0x1018U, 0U);
+	zassert_true(canopen_od_handle_is_valid(handle), "Object 1018h is present");
+	/* zassert_equal(obj->index, 0x1018, "Object index is 1018h"); */
+
+	handle = canopen_od_find(&objdict, 0x1018U, 1U);
+	zassert_true(canopen_od_handle_is_valid(handle),
+		     "Object index 1018h sub-index 1 is present");
+	/* zassert_equal(entry->subindex, 1U, "Object index 1018h sub-index 1 matches"); */
+
+	handle = canopen_od_find_by_handle(&objdict, handle, 2U);
+	zassert_true(canopen_od_handle_is_valid(handle),
+		     "Object index 1018h sub-index 2 is present");
+	/* zassert_equal(entry->subindex, 1U, "Object index 1018h sub-index 1 matches"); */
+
+	handle = canopen_od_find_by_handle(&objdict, handle, 3U);
+	zassert_true(canopen_od_handle_is_valid(handle),
+		     "Object index 1018h sub-index 3 is present");
+	/* zassert_equal(entry->subindex, 1U, "Object index 1018h sub-index 1 matches"); */
+
+	handle = canopen_od_find_by_handle(&objdict, handle, 4U);
+	zassert_true(canopen_od_handle_is_valid(handle),
+		     "Object index 1018h sub-index 4 is present");
+	/* zassert_equal(entry->subindex, 1U, "Object index 1018h sub-index 1 matches"); */
+
+	handle = canopen_od_find_by_handle(&objdict, handle, 5U);
+	zassert_false(canopen_od_handle_is_valid(handle),
+		      "Object index 1018h sub-index 5 is not present");
+}
+
+ZTEST_SUITE(canopen_od, NULL, NULL, NULL, NULL, NULL);
