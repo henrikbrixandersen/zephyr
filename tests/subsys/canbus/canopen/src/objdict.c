93e6837d1069f1f9cc41b58d275ab5dc6a9e513a
Henrik Brix Andersen <henrik@brixandersen.dk>
2021-11-15 22:42:01 +0100

    test: canbus: canopen: add test suite for the native CANopen stack
    Add unit tests for the native CANopen communication stack.
    
    Signed-off-by: Henrik Brix Andersen <henrik@brixandersen.dk>

---
+++ tests/subsys/canbus/canopen/src/objdict.c
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2021-2025 Henrik Brix Andersen <henrik@brixandersen.dk>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/canbus/canopen/od.h>
+#include <zephyr/types.h>
+
+#include "objdict.h"
+
+CANOPEN_OD_DEFINE_OBJECTS(objdict,
+	/* 1000h - Device type */
+	CANOPEN_OD_OBJECT_ENTRIES(0x1000U, CANOPEN_OD_OBJCODE_VAR,
+		CANOPEN_OD_ENTRY_UNSIGNED32(0U, &(uint32_t){ 0U }, NULL, NULL,
+					CANOPEN_OD_ATTR_ACCESS_RO),
+	),
+	/* 1001h - Error register */
+	CANOPEN_OD_OBJECT_ENTRIES(0x1001U, CANOPEN_OD_OBJCODE_VAR,
+		CANOPEN_OD_ENTRY_UNSIGNED32(0U, &(uint32_t){ 0U }, NULL, NULL,
+					CANOPEN_OD_ATTR_ACCESS_RO),
+	),
+	/* 1018h - Identity object */
+	CANOPEN_OD_OBJECT_ENTRIES(0x1018U, CANOPEN_OD_OBJCODE_RECORD,
+		CANOPEN_OD_ENTRY_UNSIGNED8(0U, &(uint8_t){ 4U }, NULL, NULL,
+					CANOPEN_OD_ATTR_ACCESS_RO),
+		CANOPEN_OD_ENTRY_UNSIGNED32(1U, &(uint32_t){ 0U }, NULL, NULL,
+					CANOPEN_OD_ATTR_ACCESS_RO),
+		CANOPEN_OD_ENTRY_UNSIGNED32(2U, &(uint32_t){ 0xdeadbeefU }, NULL, NULL,
+					CANOPEN_OD_ATTR_ACCESS_RO),
+		CANOPEN_OD_ENTRY_UNSIGNED32(3U, &(uint32_t){ 42U }, NULL, NULL,
+					CANOPEN_OD_ATTR_ACCESS_RO),
+		CANOPEN_OD_ENTRY_UNSIGNED32(4U, &(uint32_t){ 1331U }, NULL, NULL,
+					CANOPEN_OD_ATTR_ACCESS_RO),
+	),
+);
