19f2026eab54f705a4db0db8142eb42093bf264a
Henrik Brix Andersen <henrik@brixandersen.dk>
2021-11-15 22:42:01 +0100

    test: canbus: canopen: add test suite for the native CANopen stack
    Add unit tests for the native CANopen communication stack.
    
    Signed-off-by: Henrik Brix Andersen <henrik@brixandersen.dk>

---
+++ tests/subsys/canbus/canopen/src/objdict.h
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2021-2023 Henrik Brix Andersen <henrik@brixandersen.dk>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef __TEST_CANOPEN_OBJDICT_H__
+#define __TEST_CANOPEN_OBJDICT_H__
+
+#include <zephyr/canbus/canopen/od.h>
+
+CANOPEN_OD_DECLARE(objdict);
+
+#endif /* __TEST_CANOPEN_OBJDICT_H__ */
