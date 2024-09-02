b3ffbf567f6b6926dbcd417b44b82fb401b5e586
Henrik Brix Andersen <henrik@brixandersen.dk>
2024-09-02 17:40:33 +0000

    wip

---
+++ boards/adafruit/feather_m4_can_express/board.cmake
@@ -0,0 +1,6 @@
+# Copyright (c) 2024-2025 Henrik Brix Andersen <henrik@brixandersen.dk>
+# SPDX-License-Identifier: Apache-2.0
+
+include(${ZEPHYR_BASE}/boards/common/bossac.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/uf2.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
