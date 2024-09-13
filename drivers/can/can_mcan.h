12b4f74db9572171755bd3eba3f135d628b913dc
Henrik Brix Andersen <hebad@vestas.com>
2024-09-13 14:46:56 +0200

    wip2

--- drivers/can/can_mcan.h
+++ drivers/can/can_mcan.h
@@ -881,6 +881,15 @@ enum can_mcan_psr_lec {
 	CAN_MCAN_DT_MRAM_DEFINE_SECTION(DT_DRV_INST(inst), _name, _section_name)
 
 /**
+ * @brief Equivalent to CAN_MCAN_DT_MRAM_DEFINE_SECTION(DT_DRV_INST(inst), _name, _section_name)
+ * @param inst DT_DRV_COMPAT instance number
+ * @param _name buffer variable name
+ * @param _section_name Name of the linker section to place the buffer in
+ */
+#define CAN_MCAN_DT_INST_MRAM_DEFINE_SECTION(inst, _name, _section_name)                           \
+	CAN_MCAN_DT_MRAM_DEFINE_SECTION(DT_DRV_INST(inst), _name, _section_name)
+
+/**
  * @brief Bosch M_CAN specific static initializer for a minimum nominal @p can_timing struct
  */
 #define CAN_MCAN_TIMING_MIN_INITIALIZER                                                            \
